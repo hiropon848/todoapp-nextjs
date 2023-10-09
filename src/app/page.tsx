b5fd18f4d4f500a3b81c86c62e10e7f1ebd9a20d
hiropon848 <hiroakihashiba@HiroakinoMacBook-Air.local>
2023-10-09 18:53:10 +0900

    レイアウト調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,8 @@ export default async function Home() {
   console.log("todos=============");
   console.log(todos);
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-200">
-      <h1 className="text-4xl font-bold text-gray-700 -mt-32">NextJS 13 ToDo</h1>
+    <main className="flex flex-col items-center justify-start min-h-screen py-2 bg-gray-200">
+      <h1 className="text-4xl font-bold text-gray-700 -mt-0">NextJS 13 ToDo</h1>
       <div className="w-full max-w-xl mt-5">
         <div className="w-full px-8 py-6 bg-white shadow-md rounded-lg">
           <AddTask />
